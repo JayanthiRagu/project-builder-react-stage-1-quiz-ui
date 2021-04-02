3a66fdea6cb6325fa91e16e27e2e85b10af35290
Jayanthi <jayanthi.ragu93@gmail.com>
2021-04-02 15:15:32 +0530

    'first'

---
+++ quiz/src/QuizComponent/QuizComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class QuizComponent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default QuizComponent;
